fcb8e3a770165c3a5f4817474c9207fc8e8bfe0e
Eugenio Pacelli Flores <flores.eugenio03@gmail.com>
2022-10-03 14:43:21 -0500

    included: receive test & arrow function syntax

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -4,12 +4,12 @@ const { developmentChains } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
     ? describe.skip
-    : describe("FundMe", async function () {
+    : describe("FundMe", async () => {
           let fundMe
           let deployer
           let mockV3Aggregator
           const sendValue = ethers.utils.parseEther("1") // 1 ETH
-          beforeEach(async function () {
+          beforeEach(async () => {
               //deploy our fundMe contract using hardhat-deploy
               await deployments.fixture(["all"])
               deployer = (await getNamedAccounts()).deployer
@@ -19,37 +19,56 @@ const { developmentChains } = require("../../helper-hardhat-config")
                   deployer
               )
           })
-          describe("constructor", async function () {
-              it("sets the aggregator address correctly", async function () {
+          describe("constructor", async () => {
+              it("sets the aggregator address correctly", async () => {
                   const response = await fundMe.getPriceFeed()
                   assert.equal(response, mockV3Aggregator.address)
               })
           })
 
-          describe("fund", async function () {
-              it("fails if you don't send enought ETH", async function () {
+          describe("fund", async () => {
+              it("fails if you don't send enought ETH", async () => {
                   await expect(fundMe.fund()).to.be.revertedWith(
                       "You need to spend more ETH!"
                   )
               })
-              it("update the amount funded data structure", async function () {
+              it("update the amount funded data structure", async () => {
                   await fundMe.fund({ value: sendValue })
                   const response = await fundMe.getAddressToAmountFunded(
                       deployer
                   )
                   assert.equal(response.toString(), sendValue.toString())
               })
-              it("add funder to the getFunder array", async function () {
+              it("add funder to the getFunder array", async () => {
                   await fundMe.fund({ value: sendValue })
                   const funder = await fundMe.getFunder(0)
                   assert.equal(funder, deployer)
               })
           })
-          describe("withdraw", async function () {
-              beforeEach(async function () {
+
+          describe("receive", async () => {
+              beforeEach(async () => {
+                  await fundMe.signer.sendTransaction({
+                      value: sendValue,
+                      from: deployer,
+                      to: fundMe.address,
+                      gasLimit: 420000,
+                      //data: "0x90abf",
+                  })
+              })
+              it("funds the contract without calling fund", async () => {
+                  const currentBalance = await fundMe.provider.getBalance(
+                      fundMe.address
+                  )
+                  assert.equal(currentBalance.toString(), sendValue.toString())
+              })
+          })
+
+          describe("withdraw", async () => {
+              beforeEach(async () => {
                   await fundMe.fund({ value: sendValue })
               })
-              it("can withdraw from a single funder", async function () {
+              it("can withdraw from a single funder", async () => {
                   //arrange
                   const startingFundMeBalance =
                       await fundMe.provider.getBalance(fundMe.address)
@@ -76,7 +95,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
 
               //---------------------------------------------------------------------
 
-              it("allows to withdraw with multiple getFunder", async function () {
+              it("allows to withdraw with multiple getFunder", async () => {
                   //arrange
                   const accounts = await ethers.getSigners()
 
@@ -126,7 +145,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
 
               //-----------------------------------------------------------------------------
 
-              it("cheaper withdraw testing...", async function () {
+              it("cheaper withdraw testing...", async () => {
                   const accounts = await ethers.getSigners()
                   const attacker = accounts[1]
                   const attackerConnectedContract = await fundMe.connect(
@@ -136,7 +155,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
                       attackerConnectedContract.cheaperWithdraw()
                   ).to.be.revertedWith("FundMe__NotOwner")
               })
-              it("allows to withdraw with multiple getFunder", async function () {
+              it("allows to withdraw with multiple getFunder", async () => {
                   //arrange
                   const accounts = await ethers.getSigners()
 
